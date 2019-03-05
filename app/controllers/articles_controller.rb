8b4ca48a7f27892512bdf0a05d054bba14bf5f24
Khaladin <hobancor@gmail.com>
2019-03-05 11:03:34 -0500

    Add destroy nad partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,13 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Artucke was successfully destroyed"
+    redirect_to articles_path
+  end
   
   def edit
     @article = Article.find(params[:id])
